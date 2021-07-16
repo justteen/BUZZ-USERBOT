b2c25ea8dfee55d67b9f0a9b8407ba83dc9bea6b
justteen <48372964+justteen@users.noreply.github.com>
2021-07-16 13:48:15 +0700

    Add files via upload

---
+++ userbot/lightning.py
@@ -0,0 +1,22 @@
+#kang krne aaye ho  chala ja bhosdike tere kaam ka ni h ye
+#make by legendx22 for chutiapa 🤔😑 you dont kang this 
+#okay to ab isme cheeze bharte h 
+#if you read only this then okay else
+#chala jaa bhosdike 
+import os
+from userbot import ALIVE_NAME 
+MASTER = str(ALIVE_NAME) if ALIVE_NAME else "𝐁𝐔𝐙𝐙"
+NAME = os.environ.get("BOT_NICK_NAME")
+BOT =  = str(NAME) if NAME else "𝐁𝐔𝐙𝐙"
+#op for keinshin
+
+Keinshin = "[RIDWAN](https://t.me/psycho_syridwan)"
+OP = "[𝐁𝐔𝐙𝐙](https://t.me/ossuport)"
+OKAY = "[SUPPORT CH](https://t.me/supbuz)"
+#itna test h aur aage krte h
+#test successful raha ab aage 
+ALIVE = "𝐁𝐔𝐙𝐙 IS ON 🔥 FIRE 🔥" #make by LEGENDX22 for black lightning
+USERBOT = " HELLO MASTER MY NAME IS 𝐁𝐔𝐙𝐙 BOT I AM A BEST USERBOT 💝"
+EMOJI = "⚡"
+#yrr isko apne bot me aply krne se pehle mere se pooch lena ok
+#aur aage add kruga abhi busy okay 🤔
