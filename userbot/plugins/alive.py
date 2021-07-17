f897228e323e50d498d7d4ec9a7898ce473bcc84
justteen <48372964+justteen@users.noreply.github.com>
2021-07-17 08:09:53 +0700

    Add files via upload

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,227 @@
+# Thanks to Sipak bro and Aryan..
+# animation Idea by @(Sipakisking) && @Hell boy_pikachu
+# Made by @hellboi_atul ....and thanks to @Crackexy for the logos...
+# Kang with credits else gay...
+import os
+import time
+from io import BytesIO
+
+import requests
+from PIL import Image
+
+from userbot import ALIVE_NAME, CMD_HELP, StartTime, topfunc
+from userbot.Config import Var
+from userbot.thunderconfig import Config
+from userbot.utils import lightning_cmd, edit_or_reply, sudo_cmd
+
+ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
+if ALIVE_PIC is None:
+    PM_iMG = "https://telegra.ph/file/89542f076c98cc5b20c01.jpg"
+else:
+    PM_iMG = ALIVE_PIC
+
+
+HELL_PIC = os.environ.get("HELL_PIC", None)
+if HELL_PIC is None:
+    HELL_IMG = "https://telegra.ph/file/89542f076c98cc5b20c01.jpg"
+else:
+    HELL_IMG = HELL_PIC
+
+CAT_IMGE = os.environ.get("CAT_IMGE", None)
+if CAT_IMGE is None:
+    CAT_IMG = "https://telegra.ph/file/89542f076c98cc5b20c01.jpg""
+else:
+    CAT_IMG = CAT_IMGE
+
+version = "2.1"
+python_version = "3.8.5"
+catversion = "3.0"
+ALIVE_MSG = Config.ALIVE_MSG or "✮ MY BOT IS RUNNING SUCCESFULLY ✮"
+EMOJI = Config.CUSTOM_ALIVE_EMOJI or "  ✥ "
+hellversion = "7.0"
+# 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "⚡ 𝐁 𝐔 𝐙 𝐙 ⚡"
+ALIVE_PHOTTO = os.environ.get("ALIVE_PHOTTO", None)
+
+# Thanks to Sipak bro and Raganork..
+# animation Idea by @NOOB_GUY_OP (Sipakisking)
+# Made by @hellboi_atul ....and thanks to @Crackexy for the logos...
+# Kang with credits else gay...
+# alive.py for DC(DARK COBRA)
+
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+async def reply_id(event):
+    reply_to_id = None
+    if event.sender_id in Config.SUDO_USERS:
+        reply_to_id = event.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    return reply_to_id
+
+@borg.on(lightning_cmd(outgoing=True, pattern="salive"))
+@borg.on(sudo_cmd(pattern=r"salive", allow_sudo=True))
+async def amireallyalive(salive):
+    """ For .alive command, check if the bot is running.  """
+    if ALIVE_PHOTTO:
+        pm_caption = "**⚡ 𝐁 𝐔 𝐙 𝐙 ⚡ 🅾︎🅽🅻🅸🅽🅴**\n"
+        pm_caption += f"**𝕄𝕪 𝔹𝕠𝕤𝕤**            : {DEFAULTUSER}\n"
+        pm_caption += "𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽        : 1.17.5\n"
+        pm_caption += "𝙿𝚈𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽          : 3.9.0\n"
+        pm_caption += (
+            "𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙲𝙷𝙰𝙽𝙽𝙴𝙻         : [ᴊᴏɪɴ](https://t.me/supbuz)\n"
+        )
+        pm_caption += (
+            "𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿           : [ᴊᴏɪɴ](https://t.me/ossuport)\n"
+        )
+        pm_caption += "M E N C A R I J O D O H\n"
+        pm_caption += "𝘾𝙊𝙋𝙔𝙍𝙄𝙂𝙃𝙏 𝘽𝙔            : [ ⚡ 𝐁 𝐔 𝐙 𝐙 ⚡ ](https://t.me/psycho_syrid)"
+        await salive.get_chat()
+        await salive.delete()
+        """ For .allive command, check if the bot is running.  """
+        await borg.send_file(
+            salive.chat_id, ALIVE_PHOTTO, caption=pm_caption, link_preview=False
+        )
+        await sallive.delete()
+        return
+    req = requests.get("https://telegra.ph/file/89542f076c98cc5b20c01.jpg")
+    req.raise_for_status()
+    file = BytesIO(req.content)
+    file.seek(0)
+    img = Image.open(file)
+    with BytesIO() as sticker:
+        img.save(sticker, "webp")
+        sticker.name = "sticker.webp"
+        sticker.seek(0)
+        await borg.send_file(salive.chat_id, file=sticker)
+        await borg.send_message(
+            salive.chat_id,
+            "**⚡ 𝐁 𝐔 𝐙 𝐙 ⚡ 🅾︎🅽🅻🅸🅽🅴**\n"
+            f"**𝕄𝕪 𝔹𝕠𝕤𝕤**            : {DEFAULTUSER}\n"
+            "𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽        : 1.17.5\n"
+            "𝙿𝚈𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽          : 3.9.0\n"
+            "𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙲𝙷𝙰𝙽𝙽𝙴𝙻         : [ᴊᴏɪɴ](https://t.me/supbuz)\n"
+            "𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿           : [ᴊᴏɪɴ](https://t.me/ossuport)\n"
+            "M E N C A R I J O D O H."
+            "𝘾𝙊𝙋𝙔𝙍𝙄𝙂𝙃𝙏 𝘽𝙔            : [ ⚡ 𝐁 𝐔 𝐙 𝐙 ⚡ ](https://t.me/psycho_syridwan)",
+            link_preview=False,
+        )
+        await salive.delete()
+
+
+def check_data_base_heal_th():
+    # https://stackoverflow.com/a/41961968
+    is_database_working = False
+    output = "No Database is set"
+    if not Config.DB_URI:
+        return is_database_working, output
+    from userbot.plugins.sql_helper import SESSION
+
+    try:
+        # to check database we will execute raw query
+        SESSION.execute("SELECT 1")
+    except Exception as e:
+        output = f"❌ {str(e)}"
+        is_database_working = False
+    else:
+        output = "Functioning Normally"
+        is_database_working = True
+    return is_database_working, output
+
+
+# Telebot's Alive
+# Credits To Telebot And xditya
+from userbot.Config import Var
+from userbot.thunderconfig import Config
+
+CUSTOM_ALIVE = (
+    Var.CUSTOM_ALIVE
+    if Var.CUSTOM_ALIVE
+    else "Hey! I'm alive. All systems online and functioning normally!"
+)
+
+
+telemoji = Var.CUSTOM_ALIVE_EMOJI if Var.CUSTOM_ALIVE_EMOJI else "**✵**"
+
+
+if Config.SUDO_USERS:
+    sudo = "Enabled"
+else:
+    sudo = "Disabled"
+# ======CONSTANTS=========#
+
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "⚡ 𝐁 𝐔 𝐙 𝐙 ⚡"
+
+
+CMD_HELP.update(
+    {
+        "alive": "**alive**\
+        \n\n**Syntax : **`.halive For Hellbot's Alive`\
+        \n**Function : **__ Hellbot's Alive__\
+        \nFor above two commands use `.bigspam` instead of spam for spamming more than 50 messages\
+        \n\n**Syntax : **`.falive`\
+        \n**Function : **__ Fridays's Alive.__\
+        \n\n**Syntax : **`.halive `\
+        \n**Function : **__ .hell Uerbot's Alive.__\
+        \n\n**Syntax : **`.alive `\
+        \n**Function : **__ .Lightning Uerbot's Alive.__\
+        \n\n**Syntax : **`.awake `\
+        \n**Function : **__ .Awake.__\
+        \n\n\n**NOTE : All Credits To Thier Respective !!**"
+    }
+)
